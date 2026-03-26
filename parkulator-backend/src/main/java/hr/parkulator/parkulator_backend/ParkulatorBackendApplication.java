a03cb5da289eed0af84bd099199f92682bbb1610
vidhlavacek <vid7007@gmail.com>
2026-03-26 22:00:17 +0100

    Initial project files.

---
+++ parkulator-backend/src/main/java/hr/parkulator/parkulator_backend/ParkulatorBackendApplication.java
@@ -0,0 +1,13 @@
+package hr.parkulator.parkulator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkulatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkulatorBackendApplication.class, args);
+	}
+
+}
---
+++ parkulator-backend/src/test/java/hr/parkulator/parkulator_backend/ParkulatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.parkulator.parkulator_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkulatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
