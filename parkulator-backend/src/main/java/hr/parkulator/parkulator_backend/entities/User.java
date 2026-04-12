96fa1b2bbe0545e855518fb25fc9c572d1ebe286
taravidak6-svg <taravidak6@gmail.com>
2026-04-12 18:38:59 +0200

    favorites entitet

---
+++ parkulator-backend/src/main/java/hr/parkulator/parkulator_backend/entities/User.java
@@ -0,0 +1,37 @@
+package hr.parkulator.parkulator_backend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String username;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Favorite> favorites = new ArrayList<>();
+
+    public void addFavorite(Favorite favorite) {
+        favorites.add(favorite);
+        favorite.setUser(this);
+    }
+
+    public void removeFavorite(Favorite favorite) {
+        favorites.remove(favorite);
+        favorite.setUser(null);
+    }
+}
