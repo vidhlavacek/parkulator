8fc7e17117ea1a5672f2df267a845a57ec2ac6e9
atilem <lulicmelita04@gmail.com>
2026-04-14 10:11:21 +0200

    Removed lowestPrice from DTO

--- parkulator-backend/src/main/java/hr/parkulator/parkulator_backend/dto/ParkingDTO.java
+++ parkulator-backend/src/main/java/hr/parkulator/parkulator_backend/dto/ParkingDTO.java
@@ -10,7 +10,6 @@ public class ParkingDTO {
     private String name;
     private String address;
     private String type;
-    private Double lowestPrice;
     private Double highestPrice;
     private String openingHour;
     private String closingHour;
