debca7ca07320b32341a1fe510ccc75df5ff3176
vidhlavacek <vid7007@gmail.com>
2026-05-08 23:16:36 +0200

    Added scheduler for Parking data refresh

--- parkulator-backend/src/main/java/hr/parkulator/parkulator_backend/services/DataServices/ParkingDataService.java
+++ parkulator-backend/src/main/java/hr/parkulator/parkulator_backend/services/DataServices/ParkingDataService.java
@@ -3,6 +3,7 @@ package hr.parkulator.parkulator_backend.services.DataServices;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -62,7 +63,9 @@ public class ParkingDataService {
         }
     }
     
+
     @Transactional
+    @Scheduled(fixedDelay = 5000)
     public void saveRefreshData(){
         //Refreshing Parking data in the database
         //Scheduler runs this method
