ab9ccc63d0089fd93c68e66e906b9deb31f9c178
vidhlavacek <vid7007@gmail.com>
2026-05-31 17:06:11 +0200

    Removed createParkingDataDTO method

--- parkulator-backend/src/main/java/hr/parkulator/parkulator_backend/dto/parking/ParkingDataDTO.java
+++ parkulator-backend/src/main/java/hr/parkulator/parkulator_backend/dto/parking/ParkingDataDTO.java
@@ -1,11 +1,13 @@
 package hr.parkulator.parkulator_backend.dto.parking;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class ParkingDataDTO {
     private String sourceKey;
     private String name;
